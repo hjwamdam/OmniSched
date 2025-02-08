e8d9c98ae9178537407f6e47aec889e8b3430040
Henry Warmerdam <hjwamdam@gmail.com>
2025-02-07 23:36:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 //
 //  main.cpp
-//  Inheritance Assignment
+//  OmniSched: Polymorphic Appointment Planner
 //
 //  Created by Henry Warmerdam on 11/17/24.
 //
-/*
- Implement a base class Appointment and derived classes Onetime, Daily, Weekly, and Monthly. An appointment has a description (for example, “see the dentist”) and a date and time. Write a virtual function occurs_on(int year, int month, int day) that checks whether the appointment occurs on that date. For example, for a monthly appointment, you must check whether the day of the month matches. Then fill a vector of Appointment* with a mixture of appointments. Have the user enter a date and print out all appointments that happen on that date.
- 
- You will note, that I left both these questions intentionally vague. Its really just to practice getting used to virtual functions and data structures with pointers pointing to those virtual functions. I'll let you be creative, there is no wrong answer in how you approach this.
- */
 
 #include <iostream>
 #include <vector>
